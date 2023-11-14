961085279c9517088c765078536c8eb4cdf7cf93
ThaoHuynhD <thaohuynh34.678@gmail.com>
2023-11-14 11:31:08 +0700

    add adminHeader

--- src/Page/Admin/AdminHomePage.js
+++ src/Page/Admin/AdminHomePage.js
@@ -1,26 +1,19 @@
 import React, { useState } from 'react';
-import {
-    ScheduleOutlined,
-    TeamOutlined,
-} from '@ant-design/icons';
+import { ScheduleOutlined, TeamOutlined, } from '@ant-design/icons';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
 import UserManagement from './user/UserManagement';
 import PersonalPage from '../User/PersonalPage/PersonalPage';
 import CourseManagement from './course/CourseManagement';
+import { Header } from 'antd/es/layout/layout';
+import AdminHeader from '../../Components/AdminHeader/AdminHeader';
 const { Content, Sider } = Layout;
 function getItem(label, key, icon, children) {
-    return {
-        key,
-        icon,
-        children,
-        label,
-    };
+    return { key, icon, children, label, };
 }
 const items = [
     getItem('User Management', 'userManage', <TeamOutlined />),
     getItem('CourseManagement', 'courseManage', <ScheduleOutlined />),
 ];
-
 
 export default function AdminHomePage() {
     const [collapsed, setCollapsed] = useState(false);
@@ -49,6 +42,9 @@ export default function AdminHomePage() {
                 <Menu style={{ paddingTop: 10 }} theme="dark" defaultSelectedKeys={['user']} mode="inline" items={items} onClick={({ key }) => handleMenuItemClick(key)} />
             </Sider>
             <Layout>
+                <Header>
+                    <AdminHeader />
+                </Header>
                 <Content style={{ margin: '0 16px', }} >
                     <Breadcrumb style={{ margin: '16px 0', }}>
                         {breadcrumbItems}
