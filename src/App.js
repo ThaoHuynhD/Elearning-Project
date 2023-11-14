e8edf6860d2136ddedf723f0f609b4235cf40164
BAO TRANG <baogtrang@gmail.com>
2023-11-13 19:22:50 -0600

    update

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import AdminHomePage from "./Page/Admin/AdminHomePage";
 import { localServices } from "./Services/localServices";
 import CourseDetailPage from "./Page/User/CourseDetailPage/CourseDetailPage";
 import AddUser from "./Page/Admin/user/AddUser/AddUser";
-import EditUser from "./Page/Admin/user/EditUser/EditUser";
 
 function App() {
   let info = localServices.get();
@@ -85,7 +84,6 @@ function App() {
     { path: "/*", element: <NotFoundPage /> },
     { path: "/admin", element: <AdminHomePage /> },
     { path: "/addUser", element: <AddUser/>},
-    { path: `/editUser/:taiKhoan`, element: <EditUser/>}
   ];
 
   let selectedRoutes = userRoutes;
